912eb603c0e58b86a7b2448104155a272aafbb71
Meromen <urabbbc@yandex.ru>
2018-07-17 18:14:34 +1000

    ADD Gem class

--- js/game.js
+++ js/game.js
@@ -7,6 +7,49 @@ var gemHexagon = new Image();
 var gemOctagon = new Image();
 var gemRectangle = new Image();
 var gemStar = new Image();
+var GameGrid;
+
+const figureId = [gemTriangle, gemHexagon, gemOctagon, gemRectangle, gemStar];
+const gameGridSize = 8;
+const gemMinNum = 0, gemMaxNum = 5;
+const titleSize = 64;
+const CONERMARGIN = 24;
+
+class Gem {
+
+	constructor (x, y){
+		this.X = CONERMARGIN + x * titleSize;
+		this.Y = CONERMARGIN;
+		this.NeedX = this.X;
+		this.NeedY = CONERMARGIN + y * titleSize;
+		this.Kind = getRandomGem(gemMinNum, gemMaxNum);
+		this.Img = figureId[this.Kind];
+		this.Col = x;
+		this.Row = y;
+		this.Swaped = false;
+		this.Selected = false;
+	}
+
+}
+
+
+function GameGridCreate(){
+  GameGrid = new Array(gameGridSize);
+  for (let i = 0; i < GameGrid.length; i++) {
+  	GameGrid[i] = new Array(gameGridSize)
+  } 
+
+  for(let i = 0; i< gameGridSize; i++)
+  	for(let j = 0; j< gameGridSize; j++){
+  		
+  		GameGrid[i][j] = new Gem(j, i);
+
+  	} 
+}
+
+function Initialization(){
+	GameGridCreate();
+}
 
 bg.src = "img/bg.jpg";
 gemTriangle.src = "img/gem1.png";
@@ -15,14 +58,12 @@ gemOctagon.src = "img/gem3.png";
 gemRectangle.src = "img/gem4.png";
 gemStar.src = "img/gem5.png";
 
-const figureId = [gemTriangle, gemHexagon, gemOctagon, gemRectangle, gemStar];
-const gameGrid = 8;
-const gemMinNum = 0, gemMaxNum = 5;
-const titleSize = 64;
 
 function getRandomGem(min, max) {
 	return Math.floor(Math.random() * (max - min) + min);
 }
+
+
 
 function draw() {
 	ctx.drawImage(bg, 0, 0);
@@ -32,3 +73,4 @@ function draw() {
 			ctx.drawImage(figureId[getRandomGem(gemMinNum, gemMaxNum)], i * titleSize, j * titleSize);
 		}
 }
+
